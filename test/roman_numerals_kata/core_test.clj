676c1dfe605392fa1065ad8a6e8696ac04bc2915
Omar De Los Santos <omarnyte@gmail.com>
2018-06-20 16:55:14 -0700

    Initial commit

---
+++ test/roman_numerals_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns roman-numerals-kata.core-test
+  (:require [clojure.test :refer :all]
+            [roman-numerals-kata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
