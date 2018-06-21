4c1088a67e989b8763977ac8b89c3646e15e0248
Omar De Los Santos <omarnyte@gmail.com>
2018-06-21 13:36:58 -0700

    Write #should-reverse and #should-reverse-test

--- src/roman_numerals_kata/core.clj
+++ src/roman_numerals_kata/core.clj
@@ -40,17 +40,55 @@
   [num]
   (if (get arabic-roman-map num)
       true
-      false)) 
+      false))
+      
+(defn should-reverse?
+  [num]
+  (and (not (perfect-arabic-num? num))
+       (<= (- (next-up num) num)
+           (next-repeatable-down num))))
                 
+(declare convert-roman-to-arabic)
+    
+(defn convert-imperfect-arabic-num
+  [num]
+  (if (should-reverse? num)
+      (str (get arabic-roman-map (next-repeatable-down num))
+           (get arabic-roman-map(next-up num)))
+      (str (get arabic-roman-map (next-down num)) 
+           (convert-roman-to-arabic (- num 
+                                       (next-down num))))))
+    
 (defn convert-roman-to-arabic
   [num]
   (if (perfect-arabic-num? num)
       (get arabic-roman-map num)
-      (if (repeatable? num)
-          (apply str (repeat (/ num (next-repeatable-down num))
-                            (get repeatable-map 
-                                  (next-repeatable-down num))))
-          (if (= 1 (- (next-up num) num))
-              (str "I" (get arabic-roman-map 
-                            (next-up num)))
-              (str "V" (convert-roman-to-arabic (- num 5)))))))
+      (convert-imperfect-arabic-num num)))
+    
+; (declare convert-roman-to-arabic)
+
+; (defn helper-method
+;   [num]
+;   (let [next-arabic-down (next-repeatable-down num)
+;         next-repeatable-roman-down (get arabic-roman-map next-arabic-down)
+;         next-arabic-up (next-up num)
+;         next-roman-up (get arabic-roman-map next-arabic-up)]
+;     (if (repeatable? num)
+;             (apply str (repeat (/ num (next-repeatable-down num))
+;                                next-repeatable-roman-down))
+;             (if (= next-repeatable-roman-down (- next-arabic-up num))
+;                 (str next-repeatable-roman-down next-roman-up)
+;                 (str "V" (convert-roman-to-arabic (- num 5)))))))
+    
+; (defn convert-roman-to-arabic
+;   [num]
+;   (if (perfect-arabic-num? num)
+;       (get arabic-roman-map num)
+;       (helper-method num)))
+
+
+            
+; (if (= 1 (- (next-up num) num))
+;     (str "I" (get arabic-roman-map 
+;                   (next-up num)))
+;     (str "V" (convert-roman-to-arabic (- num 5)))))))
--- test/roman_numerals_kata/core_test.clj
+++ test/roman_numerals_kata/core_test.clj
@@ -2,14 +2,6 @@
   (:require [clojure.test :refer :all]
             [roman-numerals-kata.core :refer :all]))
           
-(deftest perfect-arabic-num-test
-  (is (= true (perfect-arabic-num? 1)))
-  (is (= false (perfect-arabic-num? 2)))
-  (is (= true (perfect-arabic-num? 5)))
-  (is (= false (perfect-arabic-num? 6)))
-  (is (= true (perfect-arabic-num? 10)))
-  (is (= false (perfect-arabic-num? 11))))
-          
 (deftest repeatable-test
   (is (= true (repeatable? 3)))
   (is (= false (repeatable? 4)))
@@ -17,6 +9,21 @@
   (is (= true (repeatable? 10)))
   (is (= true (repeatable? 100)))
   (is (= true (repeatable? 300))))
+
+(deftest perfect-arabic-num-test
+  (is (= true (perfect-arabic-num? 1)))
+  (is (= false (perfect-arabic-num? 2)))
+  (is (= true (perfect-arabic-num? 5)))
+  (is (= false (perfect-arabic-num? 6)))
+  (is (= true (perfect-arabic-num? 10)))
+  (is (= false (perfect-arabic-num? 11))))
+
+(deftest should-reverse-test
+  (is (= false (should-reverse? 1)))
+  (is (= true (should-reverse? 4)))
+  (is (= false (should-reverse? 5)))
+  (is (= true (should-reverse? 9)))
+  (is (= true (should-reverse? 40))))
           
 (deftest convert-roman-to-arabic-test
   (testing "translates 1 to I"
@@ -33,9 +40,9 @@
     (is (= "IX" (convert-roman-to-arabic 9))))
   (testing "translates 30 to XXX"
     (is (= "XXX" (convert-roman-to-arabic 30))))
+  (testing "translates 40 to XL"
+    (is (= "XL" (convert-roman-to-arabic 40))))
   (testing "translates 300 to CCC"
     (is (= "CCC" (convert-roman-to-arabic 300)))))
-  ; (testing "translates 40 to XL"
-  ;   (is (= "XL" (convert-roman-to-arabic 40)))))
   ; (testing "translates 48 to XLIX"
   ;   (is (= "XLIX" (convert-roman-to-arabic 49)))))
