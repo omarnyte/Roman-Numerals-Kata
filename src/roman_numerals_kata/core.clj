63473467ec22ab16fe2559036c227a25701cb5de
Omar De Los Santos <omarnyte@gmail.com>
2018-06-20 17:56:48 -0700

    Correctly converts and tests roman numbers 1 through 5

--- src/roman_numerals_kata/core.clj
+++ src/roman_numerals_kata/core.clj
@@ -1,6 +1,16 @@
 (ns roman-numerals-kata.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def roman-arabic-map 
+  { 1 "I" 5 "V" 10 "X" 50 "L" 100 "C" 500 "D" 1000 "M"})
+
+(defn next-up
+  ([num] 
+  (first (filter #(< num %) (keys roman-arabic-map)))))
+
+(defn convert-roman-to-arabic
+  [num]
+  (if (>= num 4)
+    (if (= (- (next-up num) num) 1)
+        (str "I" (get roman-arabic-map (next-up num)))
+        "V")
+    (apply str (repeat num "I"))))
--- test/roman_numerals_kata/core_test.clj
+++ test/roman_numerals_kata/core_test.clj
@@ -1,7 +1,13 @@
 (ns roman-numerals-kata.core-test
   (:require [clojure.test :refer :all]
             [roman-numerals-kata.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+          
+(deftest convert-roman-to-arabic-test
+  (testing "translates 1 to I"
+    (is (= "I" (convert-roman-to-arabic 1))))
+  (testing "translates 3 to III"
+    (is (= "III" (convert-roman-to-arabic 3))))
+  (testing "translates 4 to IV"
+    (is (= "IV" (convert-roman-to-arabic 4))))
+  (testing "translates 5 to V"
+    (is (= "V" (convert-roman-to-arabic 5)))))
