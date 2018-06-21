13d39cb4d872b65065f49d22dd797b9032f36a3d
Omar De Los Santos <omarnyte@gmail.com>
2018-06-20 18:13:41 -0700

    Create #next-down helper method

--- src/roman_numerals_kata/core.clj
+++ src/roman_numerals_kata/core.clj
@@ -5,12 +5,19 @@
 
 (defn next-up
   ([num] 
-  (first (filter #(< num %) (keys roman-arabic-map)))))
+  (first (filter #(<= num %) 
+                 (keys roman-arabic-map)))))
+
+(defn next-down
+  ([num] 
+  (last (filter #(>= num %) 
+                 (keys roman-arabic-map)))))
 
 (defn convert-roman-to-arabic
   [num]
   (if (>= num 4)
     (if (= (- (next-up num) num) 1)
         (str "I" (get roman-arabic-map (next-up num)))
-        "V")
+        (str (get roman-arabic-map (next-down num))
+             (convert-roman-to-arabic (- num (next-down num)))))
     (apply str (repeat num "I"))))
